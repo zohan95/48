6706be54c011924d7be9788fa556d06fcdaa0e73
Ваше Имя <you@example.com>
2019-09-24 11:09:49 +0600

    done stage 2

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product, cat_choices
+from .forms import SearchForm
 
-# Create your views here.
+
+def main_page(request):
+    if request.method == "GET":
+        products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
+        return render(request, 'index.html', {'products': products, 'choices': cat_choices, 'form': SearchForm})
+    elif request.method == "POST":
+        bound_form = SearchForm(request.POST)
+        if bound_form.is_valid():
+            search_query = bound_form.cleaned_data['search_query']
+            products = Product.objects.filter(name__icontains=search_query).filter(balance__gt=0).order_by('category',
+                                                                                                           'name')
+            return render(request, 'index.html', {'products': products, 'choices': cat_choices, 'form': SearchForm})
+        return redirect('main_url')
+
+
+def details_page(request, pk):
+    product = Product.objects.get(pk=pk)
+    cat = product.category
+    for i in cat_choices:
+        if cat in i:
+            cat = i[1]
+    return render(request, 'details_page.html', {'product': product, 'category': cat})
