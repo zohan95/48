2970020f8dc8f7dfc6c1ccc885ed1ede908ef869
Ваше Имя <you@example.com>
2019-09-24 13:35:38 +0600

    stage 3 done

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import Product
 
 
 class SearchForm(forms.Form):
     search_query = forms.CharField(max_length=100, label='Search')
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'description', 'category', 'balance']
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Product, cat_choices
-from .forms import SearchForm
+from .forms import SearchForm, ProductForm
 
 
 def main_page(request):
@@ -24,3 +24,37 @@ def details_page(request, pk):
         if cat in i:
             cat = i[1]
     return render(request, 'details_page.html', {'product': product, 'category': cat})
+
+
+def product_create(request):
+    if request.method == "GET":
+        return render(request, 'product_create.html', {'form': ProductForm})
+    elif request.method == "POST":
+        bound_from = ProductForm(request.POST)
+        if bound_from.is_valid():
+            bound_from.save()
+            return redirect('main_url')
+        return render(request, 'product_create.html', context={'form': bound_from})
+
+
+def product_edit(request, pk):
+    if request.method == "GET":
+        product = Product.objects.get(pk=pk)
+        form = ProductForm(instance=product)
+        return render(request, 'product_edit.html', {'form': form, 'pk1':pk})
+    elif request.method == 'POST':
+        obj = Product.objects.get(pk=pk)
+        bound_from = ProductForm(request.POST, instance=obj)
+        if bound_from.is_valid():
+            bound_from.save()
+            return redirect('main_url')
+        return render(request, 'product_edit.html', context={'form': bound_from})
+
+
+def product_delete(request, pk):
+    product = Product.objects.get(pk=pk)
+    if request.method == 'GET':
+        return render(request, 'delete.html', {'product': product})
+    elif request.method == 'POST':
+        product.delete()
+        return redirect('main_url')
