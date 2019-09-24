f19937b5b0e26f9f8b93feb34f043c4ed2b828d7
Ваше Имя <you@example.com>
2019-09-24 09:38:25 +0600

    done stage 1. created model Product

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+cat_choices = [('other', 'Разное'), ('pc', 'ПК'), ('notebook', 'Ноутбук'), ('phone', 'Телефон')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование товара')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание товара')
+    category = models.CharField(max_length=50, default='other', choices=cat_choices, verbose_name='Категория')
+    balance = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
+
+    def __str__(self):
+        return self.name
+
